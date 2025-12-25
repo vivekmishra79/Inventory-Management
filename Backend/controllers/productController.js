fd578c88fe9c769610fb316bc5722393106fabde
vivekmishrasevonmentor <vmishra@sevenmentor.services>
2025-12-26 01:39:32 +0530

    yes

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,4 +1,4 @@
-const Product = require("../models/product");
+const Product = require("../models/Product");
 const Inventory = require("../models/inventory");
 
 exports.createProduct = async (req, res) => {
