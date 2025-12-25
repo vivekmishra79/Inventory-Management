862c4a37b54a151204cba3f5a4cd3f7601a044f5
vivekmishrasevonmentor <vmishra@sevenmentor.services>
2025-12-26 02:19:42 +0530

    Fix case-sensitive Login import for Netlify

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Products from "./pages/Products";
 import Inventory from "./pages/Inventory";
